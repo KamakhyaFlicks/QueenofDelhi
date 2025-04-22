794d4935a4c211598c1d1794e94e14cbb3f7ea86
Souravkr-00 <cash.souravkr12@gmail.com>
2025-04-22 18:10:22 +0530

    changes in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// import aspect-ratio from '@tailwindcss/aspect-ratio'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
-    require('@tailwindcss/aspect-ratio')
   ],
 })
